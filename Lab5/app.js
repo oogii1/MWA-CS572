54c673dfd40a8ad6e037b5a4c8f38df0b79a8a45
Joy <oogii.joy@gmail.com>
2018-04-06 15:36:38 -0500

    Lab4 - Dooone

--- Lab5/app.js
+++ Lab5/app.js
@@ -20,8 +20,7 @@ app.get('/users', function (req, res) {
         res.render('users', { title: 'USERS - list', users: users });
     });
     const sendReq = async function (subject) {
-        const promise = fetch('http://jsonplaceholder.typicode.com/users/');
-        promise
+        fetch('http://jsonplaceholder.typicode.com/users/')
             .then(res => res.json())
             .then(json => subject.next(json));
     };
@@ -37,16 +36,12 @@ app.get('/users2', function (req, res) {
     ob.subscribe(users => res.render('users', { title: 'USERS1 - list', users: users }));
 });
 app.get('/users3', function (req, res) {
-
-    let my = new Promise((r, e) => {
-        setTimeout(() => r("====RRRRRRR===="), 2000);
-    });
-    Rx.Observable.fromPromise(my).subscribe(a => console.log(a), a => { console.log('EEERRRROOOORRR');console.log(a) });
-
-    const ob = Rx.Observable.fromPromise(fetch('http://jsonplaceholder.typicode.com/users/'));
-    ob.subscribe(response => {
-
-        res.render('users', { title: 'USERS1 - list', users: [] });
+    Rx.Observable.create(function (ob) {
+        fetch('http://jsonplaceholder.typicode.com/users/')
+            .then(res => res.json())
+            .then(users => ob.next(users));
+    }).subscribe(users => {
+        res.render('users', { title: 'USERS1 - list', users: users });
     });
 });
 
