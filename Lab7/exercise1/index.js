23d41a8ece9f1cb4fd9aa7dd05945548cf673ba6
Joy <oogii.joy@gmail.com>
2018-04-10 14:42:21 -0500

    Lab 7 - JS Format

--- Lab7/exercise1/index.js
+++ Lab7/exercise1/index.js
@@ -2,9 +2,15 @@ const mongo = require('mongodb');
 mongo.connect('mongodb://localhost:27017', function (err, client) {
     if (err) throw err;
     const db = client.db('library');
-    db.collection('books').insert({ _id: 'b1', ISBN: '123-4567', author: 'Oogii', title: 'My Book', tags: ['horror', 'novel'] });
+    db.collection('books').insert({
+        _id: 'b1',
+        ISBN: '123-4567',
+        author: 'Oogii',
+        title: 'My Book',
+        tags: ['horror', 'novel']
+    });
     db.collection('students').insert({
-        name: 'st1', 
+        name: 'st1',
         email: 'st1@mum.edu',
         phone: '99999999',
         borrowedBooks: [{
